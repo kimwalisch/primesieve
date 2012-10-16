b0c74b46bef708bcbe1c85ba13a22875578b9155
Kim Walisch <kim.walisch@gmail.com>
2012-10-16 15:20:25 +0000

    updated

--- src/test/has_openmp.cpp
+++ src/test/has_openmp.cpp
@@ -1,6 +1,9 @@
 #include <omp.h>
+#include <iostream>
 
 int main()
 {
-  return omp_get_max_threads();
+  std::cout << "OpenMP version (yyyymm) : " << _OPENMP               << std::endl
+            << "Number of CPU cores     : " << omp_get_max_threads() << std::endl;
+  return 0;
 }
