109921fd13c447f1de1e14a2a5172be9011ca3cf
Kim Walisch <kim.walisch@gmail.com>
2012-12-21 23:34:44 +0000

    simplified generate_primes_oop.cpp

--- examples/generate_primes_oop.cpp
+++ examples/generate_primes_oop.cpp
@@ -1,33 +1,27 @@
 ////////////////////////////////////////////////////////////////////
 // generate_primes_oop.cpp
-// OOP-style callback, generate the primes up to 1000 and
-// store them into the private primes_ vector.
+// This example shows how to use the generatePrimes()
+// methods with classes, the code stores the primes below
+// 1000 in the foo.primes_ vector.
 
 #include <primesieve/soe/PrimeSieve.h>
-#include <iostream>
 #include <vector>
 
 class Foo {
 public:
-  void storePrimes(unsigned int limit)
+  static void callback(unsigned int prime, void* foo)
   {
-    PrimeSieve ps;
-    ps.generatePrimes(0, limit, store, (void*) this);
-    std::cout << primes_.size() << " primes stored!" << std::endl;
+    Foo* f = (Foo*) foo;
+    f->primes_.push_back(prime);
   }
 private:
   std::vector<unsigned int> primes_;
-  /// OOP-style callback
-  static void store(unsigned int prime, void* obj)
-  {
-    Foo* f = (Foo*) obj;
-    f->primes_.push_back(prime);
-  }
 };
 
 int main()
 {
   Foo foo;
-  foo.storePrimes(1000);
+  PrimeSieve ps;
+  ps.generatePrimes(0, 1000, foo.callback, &foo);
   return 0;
 }
