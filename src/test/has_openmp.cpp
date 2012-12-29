7c9455a7cd4e84fb5337ab8ed24ef74539fe8987
Kim Walisch <kim.walisch@gmail.com>
2012-12-29 22:23:55 +0000

    shorter copyright message

--- src/test/has_openmp.cpp
+++ src/test/has_openmp.cpp
@@ -1,3 +1,13 @@
+///
+/// @file   has_openmp.cpp
+/// @brief  Used to check OpenMP support in compilers.
+///
+/// Copyright (C) 2012 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is licensed under the New BSD License. See the LICENSE
+/// file in the top-level directory.
+///
+
 #include <omp.h>
 #include <iostream>
 
