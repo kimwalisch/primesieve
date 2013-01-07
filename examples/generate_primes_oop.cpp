33d41e7d4f0a6f286daad4ab801d8d61c1c87872
Kim Walisch <kim.walisch@gmail.com>
2013-01-07 10:37:53 +0000

    uses new PrimeSieveCallback interface class

--- examples/generate_primes_oop.cpp
+++ examples/generate_primes_oop.cpp
@@ -1,27 +1,25 @@
 ////////////////////////////////////////////////////////////////////
 // generate_primes_oop.cpp
 // This example shows how to use the generatePrimes()
-// methods with classes, the code stores the primes below
-// 1000 in the foo.primes_ vector.
+// method with classes. The Foo class inherits
+// PrimeSieveCallback<T> with T = uint32_t or uint64_t.
 
 #include <primesieve/soe/PrimeSieve.h>
-#include <vector>
+#include <primesieve/soe/PrimeSieveCallback.h>
+#include <iostream>
 
-class Foo {
+class Foo : public PrimeSieveCallback<unsigned int> {
 public:
-  static void callback(unsigned int prime, void* foo)
+  void callback(unsigned int prime)
   {
-    Foo* f = (Foo*) foo;
-    f->primes_.push_back(prime);
+    std::cout << prime << '\n';
   }
-private:
-  std::vector<unsigned int> primes_;
 };
 
 int main()
 {
   Foo foo;
   PrimeSieve ps;
-  ps.generatePrimes(0, 1000, foo.callback, &foo);
+  ps.generatePrimes(0, 1000, &foo);
   return 0;
 }
