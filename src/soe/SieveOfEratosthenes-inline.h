ad9f27408cb5327d14e0d9a982baa278c32fd1d2
Kim Walisch <kim.walisch@gmail.com>
2013-02-21 15:48:02 +0000

    removed assertion

--- src/soe/SieveOfEratosthenes-inline.h
+++ src/soe/SieveOfEratosthenes-inline.h
@@ -19,34 +19,12 @@
 #include "imath.h"
 
 #include <stdint.h>
-#include <cassert>
 
 namespace soe {
 
 inline uint64_t SieveOfEratosthenes::getStart()     const { return start_; }
 inline uint64_t SieveOfEratosthenes::getStop()      const { return stop_; }
 inline uint_t   SieveOfEratosthenes::getSieveSize() const { return sieveSize_; }
-
-/// This method must be called consecutively for all primes up to
-/// sqrt(stop) in order to sieve the primes within the
-/// interval [start, stop].
-///
-inline void SieveOfEratosthenes::addSievingPrime(uint_t prime)
-{
-  assert(prime <= sqrtStop_);
-  uint64_t square = isquare<uint64_t>(prime);
-  // This loop is executed once all primes <= sqrt(segmentHigh_)
-  // required to sieve the next segment have been
-  // added to the erat* objects further down.
-  while (segmentHigh_ < square) {
-    sieveSegment();
-    segmentLow_  += sieveSize_ * NUMBERS_PER_BYTE;
-    segmentHigh_ += sieveSize_ * NUMBERS_PER_BYTE;
-  }
-       if (prime > limitEratMedium_)   eratBig_->addSievingPrime(prime, segmentLow_);
-  else if (prime > limitEratSmall_) eratMedium_->addSievingPrime(prime, segmentLow_);
-  else /* (prime > limitPreSieve) */ eratSmall_->addSievingPrime(prime, segmentLow_);
-}
 
 /// Reconstruct the prime number corresponding to the first set
 /// bit of the `bits' parameter and unset that bit.
@@ -63,6 +41,26 @@ inline uint64_t SieveOfEratosthenes::getNextPrime(uint64_t* bits, uint_t index) 
   uint64_t prime = segmentLow_ + byteValue + bitValue;
   *bits &= mask;
   return prime;
+}
+
+/// This method must be called consecutively for all primes up to
+/// sqrt(stop) in order to sieve the primes within the
+/// interval [start, stop].
+///
+inline void SieveOfEratosthenes::addSievingPrime(uint_t prime)
+{
+  uint64_t square = isquare<uint64_t>(prime);
+  // This loop is executed once all primes <= sqrt(segmentHigh_)
+  // required to sieve the next segment have been
+  // added to the erat* objects further down.
+  while (segmentHigh_ < square) {
+    sieveSegment();
+    segmentLow_  += sieveSize_ * NUMBERS_PER_BYTE;
+    segmentHigh_ += sieveSize_ * NUMBERS_PER_BYTE;
+  }
+       if (prime > limitEratMedium_)   eratBig_->addSievingPrime(prime, segmentLow_);
+  else if (prime > limitEratSmall_) eratMedium_->addSievingPrime(prime, segmentLow_);
+  else /* (prime > limitPreSieve) */ eratSmall_->addSievingPrime(prime, segmentLow_);
 }
 
 } // namespace soe
