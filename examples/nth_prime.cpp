ffd6857c2d5d21c4f45675b61a8edb0ab082c513
Kim Walisch <kim.walisch@gmail.com>
2013-03-07 08:55:51 +0000

    improved code readability

--- examples/nth_prime.cpp
+++ examples/nth_prime.cpp
@@ -9,25 +9,27 @@
 #include <limits>
 #include <exception>
 
+class stop_primesieve : public std::exception { };
+
+int n = 100000000;
 int count = 0;
-int stop = 1000;
 
 void nthprime(unsigned int prime)
 {
-  if (++count == stop) {
-    std::cout << count << "th prime = " << prime << std::endl;
-    throw std::exception();
+  if (++count == n) {
+    std::cout << n << "th prime = " << prime << std::endl;
+    throw stop_primesieve();
   }
 }
 
 int main(int, char** argv)
 {
   if (argv[1])
-    stop = atoi(argv[1]);
+    n = atoi(argv[1]);
   try {
     PrimeSieve ps;
     ps.generatePrimes(0, std::numeric_limits<int>::max(), nthprime);
   }
-  catch (std::exception&) { }
+  catch (stop_primesieve&) { }
   return 0;
 }
