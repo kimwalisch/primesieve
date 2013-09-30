9456260ae9c1f42bb44a772a3172fe2ace813f11
kim <kim@linux.fritz.box>
2013-09-30 19:51:30 +0200

    Added lazy initialization.

--- include/primesieve/soe/prime_iterator.h
+++ include/primesieve/soe/prime_iterator.h
@@ -9,16 +9,19 @@
 /// file in the top level directory.
 ///
 
-#ifndef PRIME_ITERATOR_h
-#define PRIME_ITERATOR_h
+#ifndef PRIME_ITERATOR_H
+#define PRIME_ITERATOR_H
 
 #include <vector>
 
 namespace primesieve {
 
 /// Iterate over prime numbers.
-/// This class contains 3 useful methods:
-/// prime(), next_prime() and previous_prime().
+/// This class contains 3 useful methods: prime(),
+/// next_prime() and previous_prime().
+/// @note  prime_iterator objects are very convenient to use but they
+///        are usually slower and use more memory (up to 2x) than
+///        the callback_primes() methods.
 ///
 class prime_iterator
 {
@@ -27,59 +30,68 @@ public:
   /// @param start  Start iterating at this number. If start is a
   ///               prime then first calling either next_prime()
   ///               or previous_prime() will return start.
+  /// @pre          start <= 2^64 - 2^32 * 10
   ///
   prime_iterator(uint64_t start = 0);
 
+  /// Reinitialize this prime_iterator object to start.
+  /// @param start  Start iterating at this number. If start is a
+  ///               prime then first calling either next_prime()
+  ///               or previous_prime() will return start.
+  /// @pre          start <= 2^64 - 2^32 * 10
+  ///
+  void skip_to(uint64_t start);
+
   /// Get the current prime.
-  /// @warning Returns 0 for undefined behavior e.g.:
-  ///          prime_iterator pi(/*start=*/ 4);
-  ///          pi.prime() == 0
-  /// 
+  /// @note  Returns next_prime() if neither next_prime() nor
+  ///        previous_prime() have previously been called:
+  ///        prime_iterator pi;
+  ///        pi.skip_to(4);
+  ///        pi.prime() == 5;
+  ///
   uint64_t prime()
   {
-    if (first_)
-      return first_prime();
-    return primes_[i_];
+    if (!first_)
+      return primes_[i_];
+    uint64_t next_prime = generate_next_primes();
+    return next_prime;
   }
 
   /// Get the next prime.
+  /// @return  Returns the next prime (returns 0 if
+  ///          next_prime() > 2^64 - 2^32 * 10).
+  ///
   uint64_t next_prime()
   {
-    if (first_)
-      return first_next_prime();
-    if (++i_ == primes_.size())
-      generate_next_primes();
-    return primes_[i_];
+    if (!first_ && ++i_ < size_)
+      return primes_[i_];
+    uint64_t next_prime = generate_next_primes();
+    return next_prime;
   }
 
   /// Get the previous prime.
-  /// @warning previous_prime() returns 0 below 2, e.g.:
-  ///          prime_iterator pi(/* start = */ 2);
-  ///          prime = pi.previous_prime(); /* prime == 2 */
-  ///          prime = pi.previous_prime(); /* prime == 0 */
+  /// @return  Returns the previous prime (returns 0 if
+  ///          previous_prime() < 2).
   ///
   uint64_t previous_prime()
   {
-    if (first_)
-      return first_previous_prime();
-    if (i_ == 0)
-      generate_previous_primes();
-    return primes_[--i_];
+    if (!first_ && i_ != 0)
+      return primes_[--i_];
+    uint64_t previous_prime = generate_previous_primes();
+    return previous_prime;
   }
 private:
   uint64_t i_;
-  uint64_t cache_size_;
+  uint64_t size_;
   uint64_t start_;
   bool first_;
+  bool adjust_skip_to_;
   std::vector<uint64_t> primes_;
-  void set_cache_size(uint64_t);
-  uint64_t first_prime();
-  uint64_t first_next_prime();
-  uint64_t first_previous_prime();
-  void generate_next_primes();
-  void generate_previous_primes();
+  void check_out_of_range();
+  uint64_t generate_next_primes();
+  uint64_t generate_previous_primes();
 };
 
-}// end namespace
+} // end namespace
 
 #endif
--- src/soe/prime_iterator.cpp
+++ src/soe/prime_iterator.cpp
@@ -9,121 +9,139 @@
 
 #include <primesieve.h>
 #include <primesieve/soe/prime_iterator.h>
+#include <primesieve/soe/primesieve_error.h>
+#include <primesieve/soe/PrimeFinder.h>
+
 #include <algorithm>
 #include <cmath>
+#include <string>
 #include <vector>
+
+using soe::PrimeFinder;
 
 namespace {
 
-uint64_t megabytes(uint64_t n)
-{
-  return n << 20;
-}
+// MAX_STOP = 2^64 - 2^32 * 10
+const uint64_t MAX_STOP = PrimeFinder::getMaxStop();
 
-/// Estimate interval size based on number of primes.
-/// @param n       start or stop number.
-/// @param primes  Number of primes to generate.
+/// Calculate an interval size that ensures a good load balance.
+/// @param n  start or stop number.
 ///
-uint64_t get_interval_size(uint64_t n, uint64_t primes)
+uint64_t get_interval_size(uint64_t n)
 {
-  const uint64_t MIN_INTERVAL = 1 << 16;
-  n = std::max(n, primes * 20);
-  double dn = static_cast<double>(n);
-  double logn = std::log(dn);
-  uint64_t interval_size = static_cast<uint64_t>(primes * logn);
-  return std::max(MIN_INTERVAL, interval_size);
+  const uint64_t MEGABYTE = 1 << 20;
+  double dn = std::max(static_cast<double>(n), 100.0);
+  double sqrtn = std::sqrt(dn);
+  uint64_t primes = static_cast<uint64_t>(sqrtn / (std::log(sqrtn) - 1.0));
+
+  // lower limit = 2 megabytes
+  if (primes * sizeof(uint64_t) < MEGABYTE * 2)
+   primes = MEGABYTE * 2 / sizeof(uint64_t);
+  // upper limit = 1 gigabyte
+  if (primes * sizeof(uint64_t) > MEGABYTE * 512)
+   primes = MEGABYTE * 512 / sizeof(uint64_t);
+
+  uint64_t interval_size = static_cast<uint64_t>(primes * std::log(dn));
+  return interval_size;
 }
 
 } // end namespace
 
 namespace primesieve {
 
-prime_iterator::prime_iterator(uint64_t start) :
-  start_(start),
-  first_(true)
+prime_iterator::prime_iterator(uint64_t start)
 {
-  set_cache_size(start_);
-  uint64_t interval_size = get_interval_size(start_, cache_size_);
-  uint64_t mid = interval_size / 2;
-  uint64_t begin = (start_ > mid) ? start_ - mid : 0;
-
-  generate_n_primes(cache_size_, begin, &primes_);
-  cache_size_ = primes_.size();
-  i_ = std::lower_bound(primes_.begin(), primes_.end(), start_) - primes_.begin();
+  skip_to(start);
 }
 
-/// Each time new primes have to be generated the cache
-/// size is dynamically grown or shrunk.
+void prime_iterator::skip_to(uint64_t start)
+{
+  start_ = start;
+  first_ = true;
+  adjust_skip_to_ = false;
+
+  if (start_ > MAX_STOP)
+    throw primesieve_error("start must be <= " + PrimeFinder::getMaxStopString());
+
+  if (!primes_.empty() &&
+       primes_.front() <= start_ &&
+       primes_.back() >= start_)
+  {
+    adjust_skip_to_ = true;
+    i_ = std::lower_bound(primes_.begin(), primes_.end(), start_)
+        - primes_.begin();
+  }
+}
+
+/// Return 0 if out of range, e.g.:
+/// prime_iterator pi;
+/// pi.skip_to(1);
+/// pi.previous_prime() == 0;
 ///
-void prime_iterator::set_cache_size(uint64_t n)
+void prime_iterator::check_out_of_range()
 {
-  // estimate number of primes <= sqrt(n)
-  double dn = std::max(static_cast<double>(n), 100.0);
-  cache_size_ = static_cast<uint64_t>(std::sqrt(dn) / (std::log(dn) - 1.0));
-
-  // lower limit = 2 megabytes
-  if (cache_size_ * sizeof(uint64_t) < megabytes(2))
-   cache_size_ = megabytes(2) / sizeof(uint64_t);
-
-  // upper limit = 1 gigabyte
-  if (cache_size_ * sizeof(uint64_t) > megabytes(1024))
-   cache_size_ = megabytes(1024) / sizeof(uint64_t);
-}
-
-uint64_t prime_iterator::first_prime()
-{
-  // undefined behavior
-  if (primes_[i_] != start_)
-    return 0;
-
-  first_ = false;
-  return primes_[i_];
-}
-
-uint64_t prime_iterator::first_next_prime()
-{
-  first_ = false;
-  if (primes_[i_] < start_)
-    i_++;
-  return primes_[i_];
-}
-
-uint64_t prime_iterator::first_previous_prime()
-{
-  first_ = false;
-  if (primes_[i_] > start_)
-    i_--;
-  return primes_[i_];
-}
-
-void prime_iterator::generate_next_primes()
-{
-  set_cache_size(primes_.back());
-  uint64_t start = primes_.back() + 1;
-  primes_.clear();
-  generate_n_primes(cache_size_, start, &primes_);
-  cache_size_ = primes_.size();
-  i_ = 0;
-}
-
-void prime_iterator::generate_previous_primes()
-{
-  uint64_t stop = primes_.front();
-  if (stop > 1)
-    stop--;
-
-  set_cache_size(primes_.front());
-  uint64_t interval_size = get_interval_size(stop, cache_size_);
-  uint64_t start = 0;
-  if (stop > interval_size)
-    start = stop - interval_size;
-
-  primes_.clear();
-  generate_primes(start, stop, &primes_);
-
   if (primes_.empty())
     primes_.push_back(0);
-  i_ = cache_size_ = primes_.size();
+}
+
+/// Generate new next primes.
+/// @return  next prime.
+///
+uint64_t prime_iterator::generate_next_primes()
+{
+  if (adjust_skip_to_)
+  {
+    adjust_skip_to_ = false;
+    if (i_ + 1 < primes_.size() && primes_[i_] < start_)
+      i_++;
+  }
+  else
+  {
+    uint64_t start = start_;
+    if (!first_)
+      start = primes_.back() + 1;
+    uint64_t interval_size = get_interval_size(start);
+    uint64_t stop = MAX_STOP;
+    if (start < MAX_STOP - interval_size)
+      stop = start + interval_size;
+    primes_.clear();
+    generate_primes(start, stop, &primes_);
+    check_out_of_range();
+    i_ = 0;
+  }
+  first_ = false;
+  size_ = primes_.size();
+  return primes_[i_];
+}
+
+/// Generate new previous primes.
+/// @return  previous prime.
+///
+uint64_t prime_iterator::generate_previous_primes()
+{
+  if (adjust_skip_to_)
+  {
+    adjust_skip_to_ = false;
+    if (i_ > 0 && primes_[i_] > start_)
+      i_--;
+  }
+  else
+  {
+    uint64_t stop = start_;
+    if (!first_)
+      stop = (primes_.front() > 1) ? primes_.front() - 1 : 0;
+    uint64_t interval_size = get_interval_size(stop);
+    uint64_t start = 0;
+    if (stop > interval_size)
+      start = stop - interval_size;
+    primes_.clear();
+    generate_primes(start, stop, &primes_);
+    check_out_of_range();
+    i_ = primes_.size() - 1;
+  }
+  first_ = false;
+  size_ = primes_.size();
+  return primes_[i_];
 }
 
 } // end namespace
