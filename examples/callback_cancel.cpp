9ddf11764e9bfb7ed45bd3cd0b1bfd15cada4dd0
kim <kim@linux.fritz.box>
2013-09-30 20:41:52 +0200

    Refactoring.

--- examples/callback_cancel.cpp
+++ examples/callback_cancel.cpp
@@ -1,29 +1,27 @@
 ////////////////////////////////////////////////////////////////////
 // cancel_callback.cpp
 // Cancel prime callback by throwing a stop_primesieve()
-// exception. Note that multi-threaded callback can currently
-// not be canceled. This code finds the 10^7th prime.
+// exception. This code finds the 10^6th prime.
+// Note that multi-threaded callback can currently not be canceled.
 
 #include <primesieve.h>
 #include <primesieve/soe/stop_primesieve.h>
 #include <iostream>
 
 int i = 0;
-int n = 10000000;
 
 void callback(unsigned int prime)
 {
-  if (++i == n) {
-    std::cout << n << "th prime = " << prime << std::endl;
+  if (++i == 1000000) {
+    std::cout << "10^6th prime = " << prime << std::endl;
     throw stop_primesieve();
   }
 }
 
 int main()
 {
-  try
-  {
-    primesieve::callback_primes(0, n * 50, callback);
+  try {
+    primesieve::callback_primes(0, 999999999, callback);
   }
   catch (stop_primesieve&) { }
   return 0;
