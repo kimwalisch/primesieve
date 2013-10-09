6e275b476d1e4e4c966237325d57327323d3153f
Kim Walisch <kim.walisch@gmail.com>
2013-10-09 20:47:11 +0200

    Big fix: use uint64_t instead of int

--- examples/callback_primes_oop.cpp
+++ examples/callback_primes_oop.cpp
@@ -1,14 +1,15 @@
 /// @example callback_primes_oop.cpp
-/// Objects derived from PrimeSieveCallback<T> can be
+/// Objects derived from PrimeSieveCallback<uint64_t> can be
 /// passed to the callback_primes() function.
 
 #include <primesieve.h>
+#include <stdint.h>
 #include <list>
 
-struct PrimeList : std::list<int>,
-                   PrimeSieveCallback<int>
+struct PrimeList : std::list<uint64_t>,
+                   PrimeSieveCallback<uint64_t>
 {
-  void callback(int prime) {
+  void callback(uint64_t prime) {
     this->push_back(prime);
   }
 };
