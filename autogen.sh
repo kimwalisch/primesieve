fd24ec89207615a20bcbb2fe147b8ea9a67209c3
kim <kim.walisch@gmail.com>
2013-10-17 20:25:45 +0200

    Use 0 if nano is not specified

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,9 @@ command -v libtool >/dev/null 2>/dev/null
 if [ $? -eq 0 ]
 then
     libtool_version=$(libtool --version 2>/dev/null | head -n1 | cut -d' ' -f4)
-    libtool1=$(echo $libtool_version | cut -d'.' -f1)
-    libtool2=$(echo $libtool_version | cut -d'.' -f2)
-    libtool3=$(echo $libtool_version | cut -d'.' -f3)
+    libtool1=$(echo 0$libtool_version | cut -d'.' -f1)
+    libtool2=$(echo 0$libtool_version | cut -d'.' -f2)
+    libtool3=$(echo 0$libtool_version | cut -d'.' -f3)
     libtool_version=$(expr $libtool1 '*' 1000000 '+' $libtool2 '*' 1000 '+' $libtool3 2>/dev/null)
     if [ "$libtool" != "" ] && [ $libtool_version -lt 2004002 ]
     then
