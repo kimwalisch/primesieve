0c9217b0edcf7f5fd3039156fce36683656084ac
kim <kim.walisch@gmail.com>
2013-10-17 21:11:18 +0200

    Improved Libtool patch

--- autogen.sh
+++ autogen.sh
@@ -7,13 +7,13 @@ command -v libtool >/dev/null 2>/dev/null
 if [ $? -eq 0 ]
 then
     libtool_version=$(libtool --version 2>/dev/null | head -n1 | cut -d' ' -f4)
-    libtool1=$(echo $libtool_version | cut -d'.' -f1)
-    libtool2=$(echo $libtool_version | cut -d'.' -f2)
-    libtool3=$(echo $libtool_version | cut -d'.' -f3)
-    if [ "$libtool1" != "" ]; then libtool_version=$(expr $libtool1 '*' 1000000 2>/dev/null); fi
-    if [ "$libtool2" != "" ]; then libtool_version=$(expr $libtool_version '+' $libtool2 '*' 1000 2>/dev/null); fi
-    if [ "$libtool3" != "" ]; then libtool_version=$(expr $libtool_version '+' $libtool3 2>/dev/null); fi
-    if [ "$libtool_version" != "" ] && [ $libtool_version -lt 2004002 ]
+    libtool_f1=$(echo "$libtool_version" | cut -d'.' -f1)
+    libtool_f2=$(echo "$libtool_version" | cut -d'.' -f2)
+    libtool_f3=$(echo "$libtool_version" | cut -d'.' -f3)
+    if [ "$libtool_f1" != "" ]; then libtool_nr=$(expr "$libtool_f1" '*' 1000000 2>/dev/null); fi
+    if [ "$libtool_f2" != "" ]; then libtool_nr=$(expr "$libtool_nr" '+' "$libtool_f2" '*' 1000 2>/dev/null); fi
+    if [ "$libtool_f3" != "" ]; then libtool_nr=$(expr "$libtool_nr" '+' "$libtool_f3" 2>/dev/null); fi
+    if [ "$libtool_nr" -lt 2004002 2>/dev/null ]
     then
         sed 's/|-threads)/-threads|-fopenmp|-openmp|-mp|-xopenmp|-omp|-qsmp=*)/g' ltmain.sh > ltmain2.sh
         mv -f ltmain2.sh ltmain.sh
