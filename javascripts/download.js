dbc1e66642833975b3a2c8446aaf0290a8c457cd
Kim Walisch <kim.walisch@gmail.com>
2014-01-08 17:24:55 +0100

    Create download.js

---
+++ javascripts/download.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var primesieve_download_url = "http://dl.bintray.com/kimwalisch/primesieve/primesieve-";
+	var primesieve_version = 4.4;
+	$("a#download_zip").attr("href", primesieve_download_url + primesieve_version + ".zip");
+	$("a#download_targz").attr("href", primesieve_download_url + primesieve_version + ".tar.gz");
+});
