9db8e8d137fada8d90f293dda2aba2844b65ac91
Kim Walisch <kim.walisch@gmail.com>
2014-01-08 17:24:18 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,1 @@
 console.log('This would be the main JS file.');
-var primesieve_version = 4.4;
-var primesieve_bintray = "http://dl.bintray.com/kimwalisch/primesieve/primesieve-";
-document.getElementById("download_primesieve_release_targz").setAttribute("href", primesieve_bintray + primesieve_version ".tar.gz");
-document.getElementById("download_primesieve_release_zip").setAttribute("href", primesieve_bintray + primesieve_version + ".zip");
