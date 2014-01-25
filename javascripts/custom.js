138ddbbe086726c869beab5cab8f5ddbdc4c61b1
Kim Walisch <kim.walisch@gmail.com>
2014-01-25 19:31:44 +0100

    svg fallback mode using modernizer.js
    code in javascripts/custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
 	$("a.download_targz").attr("href", download_url + primesieve_version + ".tar.gz");
 });
 
+if(!Modernizr.svg) {
+  $('img[src$="svg"]').attr('src', function() {
+    return $(this).attr('src').replace('.svg', '.png');
+  });
+}
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
