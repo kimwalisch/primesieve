001381da53f2cb5e87d07f6d6a8068fa7d114a04
Kim Walisch <kim.walisch@gmail.com>
2014-02-05 13:54:34 +0100

    Remove binary search
    Binary search is not really useful here

--- src/primesieve/iterator.cpp
+++ src/primesieve/iterator.cpp
@@ -40,29 +40,23 @@ void iterator::set_stop_hint(uint64_t stop_hint)
   stop_hint_ = stop_hint;
 }
 
+void iterator::clear()
+{
+  skipto(0);
+}
+
 void iterator::skipto(uint64_t start, uint64_t stop_hint)
 {
   if (start_ > get_max_stop())
     throw primesieve_error("start must be <= " + PrimeFinder::getMaxStopString());
 
-  i_ = 0;
-  count_ = 0;
   start_ = start;
   stop_ = start;
-  set_stop_hint(stop_hint);
-  first_ = true;
-  is_binary_search_ = (!primes_.empty() &&
-                        primes_.front() <= start_ &&
-                        primes_.back() >= start_);
-
-  if (!is_binary_search_)
-    primes_.clear();
-}
-
-void iterator::clear()
-{
+  stop_hint_ = stop_hint;
+  i_ = 0;
+  last_idx_ = 0;
+  count_ = 0;
   primes_.clear();
-  skipto(0);
 }
 
 uint64_t add_overflow_safe(uint64_t a, uint64_t b)
@@ -78,64 +72,48 @@ uint64_t subtract_underflow_safe(uint64_t a, uint64_t b)
 
 void iterator::generate_next_primes()
 {
-  if (is_binary_search_)
-  {
-    first_ = false;
-    is_binary_search_ = false;
-    i_ = std::lower_bound(primes_.begin(), primes_.end(), start_) - primes_.begin();
-  }
-  else
-  {
-    primes_.clear();
-    uint64_t max_stop = get_max_stop();
+  bool first = primes_.empty();
+  uint64_t max_stop = get_max_stop();
+  primes_.clear();
 
-    while (primes_.empty())
-    {
-      if (!first_)
-        start_ = add_overflow_safe(stop_, 1);
-      first_ = false;
-      stop_ = add_overflow_safe(start_, get_interval_size(start_));
-      if (start_ <= stop_hint_ && stop_ >= stop_hint_)
-        stop_ = add_overflow_safe(stop_hint_, max_prime_gap(stop_hint_));
-      primesieve::generate_primes(start_, stop_, &primes_);
-      if (primes_.empty() && stop_ >= max_stop)
-        throw primesieve_error("next_prime() > " + PrimeFinder::getMaxStopString());
-    }
-
-    i_ = 0;
+  while (primes_.empty())
+  {
+    if (!first)
+      start_ = add_overflow_safe(stop_, 1);
+    first = false;
+    stop_ = add_overflow_safe(start_, get_interval_size(start_));
+    if (start_ <= stop_hint_ && stop_ >= stop_hint_)
+      stop_ = add_overflow_safe(stop_hint_, max_prime_gap(stop_hint_));
+    primesieve::generate_primes(start_, stop_, &primes_);
+    if (primes_.empty() && stop_ >= max_stop)
+      throw primesieve_error("next_prime() > " + PrimeFinder::getMaxStopString());
   }
+
+  last_idx_ = primes_.size() - 1;
+  i_ = 0;
 }
 
 void iterator::generate_previous_primes()
 {
-  if (is_binary_search_)
-  {
-    first_ = false;
-    is_binary_search_ = false;
-    i_ = std::lower_bound(primes_.begin(), primes_.end(), start_) - primes_.begin();
-    if (i_ > 0 && primes_[i_] > start_)
-      i_--;
-  }
-  else
-  {
-    primes_.clear();
+  bool first = primes_.empty();
+  primes_.clear();
 
-    while (primes_.empty())
-    {
-      if (!first_)
-        stop_ = subtract_underflow_safe(start_, 1);
-      first_ = false;
-      uint64_t interval_size = get_interval_size(stop_);
-      start_ = subtract_underflow_safe(stop_, interval_size);
-      if (start_ <= stop_hint_ && stop_ >= stop_hint_)
-        start_ = subtract_underflow_safe(stop_hint_, max_prime_gap(stop_hint_));
-      primesieve::generate_primes(start_, stop_, &primes_);
-      if (primes_.empty() && start_ < 2)
-        throw primesieve_error("previous_prime(): smallest prime is 2");
-    }
-
-    i_ = primes_.size() - 1;
+  while (primes_.empty())
+  {
+    if (!first)
+      stop_ = subtract_underflow_safe(start_, 1);
+    first = false;
+    uint64_t interval_size = get_interval_size(stop_);
+    start_ = subtract_underflow_safe(stop_, interval_size);
+    if (start_ <= stop_hint_ && stop_ >= stop_hint_)
+      start_ = subtract_underflow_safe(stop_hint_, max_prime_gap(stop_hint_));
+    primesieve::generate_primes(start_, stop_, &primes_);
+    if (primes_.empty() && start_ < 2)
+      throw primesieve_error("previous_prime(): smallest prime is 2");
   }
+
+  last_idx_ = primes_.size() - 1;
+  i_ = last_idx_;
 }
 
 /// Calculate an interval size that ensures a good load balance.
