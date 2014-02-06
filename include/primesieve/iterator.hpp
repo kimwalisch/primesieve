3b922d08a1848ee0dfc86c0375e5067799f5a3dd
Kim Walisch <kim.walisch@gmail.com>
2014-02-06 08:53:38 +0100

    Refactoring

--- include/primesieve/iterator.hpp
+++ include/primesieve/iterator.hpp
@@ -50,12 +50,6 @@ public:
   ///
   void skipto(uint64_t start, uint64_t stop_hint = get_max_stop());
 
-  /// Set a stop number optimization hint. Setting stop_hint gives
-  /// a significant speed up if only few primes are generated.
-  /// @pre  stop_hint <= 2^64 - 2^32 * 10
-  ///
-  void set_stop_hint(uint64_t stop_hint);
-
   /// Advance the iterator by one position.
   /// @return  The next prime.
   ///
