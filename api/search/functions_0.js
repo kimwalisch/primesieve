76e377ccc09385b6e1fdc75b4c6abdcb0744ff56
Kim Walisch <kim.walisch@gmail.com>
2014-02-12 22:42:41 +0100

    Update to version 5.1

---
+++ api/search/functions_0.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['callback_5fprimes',['callback_primes',['../namespaceprimesieve.html#a12f86ad1b5a5c0aac724c35ff02ad49f',1,'primesieve::callback_primes(uint64_t start, uint64_t stop, void(*callback)(uint64_t prime))'],['../namespaceprimesieve.html#a32afb0179475d78263f9adce476270b9',1,'primesieve::callback_primes(uint64_t start, uint64_t stop, primesieve::Callback&lt; uint64_t &gt; *callback)']]],
+  ['clear',['clear',['../classprimesieve_1_1iterator.html#a2e9d12b6bf62b0a153dd7bbf1ffede77',1,'primesieve::iterator']]],
+  ['count_5fprimes',['count_primes',['../namespaceprimesieve.html#a6e8caa0de4a20596b0fe8b4a9f0f3f1a',1,'primesieve']]],
+  ['count_5fquadruplets',['count_quadruplets',['../namespaceprimesieve.html#aa5122db2b01e8495d80b19d14d309d04',1,'primesieve']]],
+  ['count_5fquintuplets',['count_quintuplets',['../namespaceprimesieve.html#a2659c2cae674ff90d23af133a05b2fd2',1,'primesieve']]],
+  ['count_5fseptuplets',['count_septuplets',['../namespaceprimesieve.html#a83151165b9a96f5ab1ec9c6683a1ab7b',1,'primesieve']]],
+  ['count_5fsextuplets',['count_sextuplets',['../namespaceprimesieve.html#ab0e3ba832781b6b50d249aa3ebdaa78a',1,'primesieve']]],
+  ['count_5ftriplets',['count_triplets',['../namespaceprimesieve.html#a271664ff948a9c109acdef49ec761be4',1,'primesieve']]],
+  ['count_5ftwins',['count_twins',['../namespaceprimesieve.html#ad8fd139e642949b604e8887e436e3c90',1,'primesieve']]]
+];
--- api/search/functions_69.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['iterator',['iterator',['../classprimesieve_1_1iterator.html#afc60bc63dcfa508839fc024d7815d53e',1,'primesieve::iterator']]]
-];
