36cdbc772887fa9cb17538ade7e465bd137c7165
Kim Walisch <kim.walisch@gmail.com>
2014-02-13 09:50:14 +0100

    Remove clear()

--- api/search/all_0.js
+++ api/search/all_0.js
@@ -4,7 +4,6 @@ var searchData=
   ['callback_2ehpp',['Callback.hpp',['../Callback_8hpp.html',1,'']]],
   ['callback_3c_20uint64_5ft_2c_20int_20_3e',['Callback&lt; uint64_t, int &gt;',['../classprimesieve_1_1Callback_3_01uint64__t_00_01int_01_4.html',1,'primesieve']]],
   ['callback_5fprimes',['callback_primes',['../namespaceprimesieve.html#a12f86ad1b5a5c0aac724c35ff02ad49f',1,'primesieve::callback_primes(uint64_t start, uint64_t stop, void(*callback)(uint64_t prime))'],['../namespaceprimesieve.html#a32afb0179475d78263f9adce476270b9',1,'primesieve::callback_primes(uint64_t start, uint64_t stop, primesieve::Callback&lt; uint64_t &gt; *callback)']]],
-  ['clear',['clear',['../classprimesieve_1_1iterator.html#a2e9d12b6bf62b0a153dd7bbf1ffede77',1,'primesieve::iterator']]],
   ['count_5fprimes',['count_primes',['../namespaceprimesieve.html#a6e8caa0de4a20596b0fe8b4a9f0f3f1a',1,'primesieve']]],
   ['count_5fquadruplets',['count_quadruplets',['../namespaceprimesieve.html#aa5122db2b01e8495d80b19d14d309d04',1,'primesieve']]],
   ['count_5fquintuplets',['count_quintuplets',['../namespaceprimesieve.html#a2659c2cae674ff90d23af133a05b2fd2',1,'primesieve']]],
