321839df4c6ffb8703ea9c4a24fbf82102e93060
Kim Walisch <kim.walisch@gmail.com>
2014-02-14 20:14:22 +0100

    Release new primesieve-5.1 source archives

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var primesieve_version = "5.0";
+	var primesieve_version = "5.1";
 	var download_url = "http://dl.bintray.com/kimwalisch/primesieve/primesieve-";
 	$("a.download_zip").attr("href", download_url + primesieve_version + ".zip");
 	$("a.download_targz").attr("href", download_url + primesieve_version + ".tar.gz");
