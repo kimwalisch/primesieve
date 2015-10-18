d18ad8973e42429f586f5bdcfc2096e0bed43401
Kim Walisch <kim.walisch@gmail.com>
2015-10-18 16:44:10 +0200

    Increase version to 5.5.0

--- include/primesieve.h
+++ include/primesieve.h
@@ -14,10 +14,10 @@
 #ifndef PRIMESIEVE_H
 #define PRIMESIEVE_H
 
-#define PRIMESIEVE_VERSION "5.4.2"
+#define PRIMESIEVE_VERSION "5.5.0"
 #define PRIMESIEVE_VERSION_MAJOR 5
-#define PRIMESIEVE_VERSION_MINOR 4
-#define PRIMESIEVE_VERSION_PATCH 2
+#define PRIMESIEVE_VERSION_MINOR 5
+#define PRIMESIEVE_VERSION_PATCH 0
 
 #include <primesieve/primesieve_iterator.h>
 #include <stdint.h>
--- include/primesieve.hpp
+++ include/primesieve.hpp
@@ -15,10 +15,10 @@
 #ifndef PRIMESIEVE_HPP
 #define PRIMESIEVE_HPP
 
-#define PRIMESIEVE_VERSION "5.4.2"
+#define PRIMESIEVE_VERSION "5.5.0"
 #define PRIMESIEVE_VERSION_MAJOR 5
-#define PRIMESIEVE_VERSION_MINOR 4
-#define PRIMESIEVE_VERSION_PATCH 2
+#define PRIMESIEVE_VERSION_MINOR 5
+#define PRIMESIEVE_VERSION_PATCH 0
 
 #include "primesieve/PrimeSieve.hpp"
 #include "primesieve/ParallelPrimeSieve.hpp"
