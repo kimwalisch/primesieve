3e922661d43c50339ec14d50b2890fc59ec20dee
Kim Walisch <kim.walisch@gmail.com>
2015-10-18 15:31:44 +0200

    Update help menu

--- src/apps/console/help.cpp
+++ src/apps/console/help.cpp
@@ -21,18 +21,18 @@ namespace {
 
 const string helpMenu(
   "Usage: primesieve [START] STOP [OPTION]...\n"
-  "Use the segmented sieve of Eratosthenes to generate the primes and\n"
-  "prime k-tuplets in the interval [START, STOP] < 2^64\n"
+  "Generate the primes and/or prime k-tuplets in the interval [START, STOP]\n"
+  "(< 2^64) using the segmented sieve of Eratosthenes.\n"
   "\n"
   "Options:\n"
-  "  -c<N*>, --count=<N*>     Count primes and prime k-tuplets, 1 <= N <= 6\n"
-  "                           <N> 1=primes, 2=twins, 3=triplets, ...\n"
+  "  -c[N+], --count[=N+]     Count primes and prime k-tuplets, 1 <= N <= 6\n"
+  "                           N: 1=primes, 2=twins, 3=triplets, ...\n"
   "  -h,     --help           Print this help menu\n"
   "  -n,     --nthprime       Calculate the nth prime\n"
   "                           e.g. 1 100 -n finds the first prime > 100\n"
   "  -o<N>,  --offset=<N>     Sieve the interval [START, START + N]\n"
-  "  -p<N?>, --print=<N?>     Print primes or prime k-tuplets, 1 <= N <= 6\n"
-  "                           <N> 1=primes, 2=twins, 3=triplets, ...\n"
+  "  -p[N],  --print[=N]      Print primes or prime k-tuplets, 1 <= N <= 6\n"
+  "                           N: 1=primes, 2=twins, 3=triplets, ...\n"
   "  -q,     --quiet          Quiet mode, prints less output\n"
   "  -s<N>,  --size=<N>       Set the sieve size in kilobytes, 1 <= N <= 2048\n"
   "          --test           Run various sieving tests and exit\n"
@@ -43,14 +43,8 @@ const string helpMenu(
   "  Count and print the twin primes below 1000000\n"
   "  > primesieve 1e6 --count=2 -p2\n"
   "\n"
-  "  Count the primes inside the interval [10^9, 10^9+2^32]\n"
-  "  > primesieve 1e9 1e9+2**32 --count"
-);
-
-const string versionInfo(
-  "primesieve " PRIMESIEVE_VERSION ", <http://primesieve.org>\n"
-  "Copyright (C) 2015 Kim Walisch\n"
-  "BSD 2-Clause License <http://opensource.org/licenses/BSD-2-Clause>"
+  "  Print the primes inside the interval [10^9, 10^9+2^32]\n"
+  "  > primesieve 1e9 1e9+2**32 --print"
 );
 
 } // end namespace
@@ -63,6 +57,10 @@ void help()
 
 void version()
 {
-  cout << versionInfo << endl;
+  cout << "primesieve " << primesieve::primesieve_version();
+  cout << ", <http://primesieve.org>" << endl;
+  cout << "Copyright (C) 2015 Kim Walisch" << endl;
+  cout << "BSD 2-Clause License <http://opensource.org/licenses/BSD-2-Clause>" << endl;
+
   exit(1);
 }
