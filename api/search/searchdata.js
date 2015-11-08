06f890de2bfc342ef2efe3bd3049b44f1ac5d66a
Kim Walisch <kim.walisch@gmail.com>
2015-11-08 13:37:58 +0100

    Update API to version 5.5.0

---
+++ api/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "cgilmnpsu",
+  1: "cinp",
+  2: "p",
+  3: "cip",
+  4: "cginps",
+  5: "ilmsu",
+  6: "p"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "enumvalues",
+  6: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Enumerator",
+  6: "Macros"
+};
+
