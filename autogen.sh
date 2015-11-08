0c2e7fe34849554b9ec239950e1f2062cc30c620
Kim Walisch <kim.walisch@gmail.com>
2015-11-08 01:11:39 +0100

    Fix kFreeBsd bug
    Using AC_CONFIG_MACRO_DIRS only works if autoconf >= 2.69 and automake
    >= 1.13. Hence I revert back to the old AC_CONFIG_MACRO_DIR which does
    not have this issue.

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-# This code patches configure.ac (replaces AC_CONFIG_MACRO_DIRS
-# by AC_CONFIG_MACRO_DIR) if autoconf version < 2.69
-command -v autoconf >/dev/null 2>/dev/null
-if [ $? -eq 0 ]
-then
-    autoconf_version=$(autoconf --version 2>/dev/null | head -n1 2>/dev/null | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' 2>/dev/null)
-    if [ "x$autoconf_version" != "x" ];
-    then
-        is_patch=$(echo $autoconf_version 2.69 | awk '{print ($1 < $2)}' 2>/dev/null)
-        if [ "$is_patch" = "1" ];
-        then
-            sed 's/AC_CONFIG_MACRO_DIRS/AC_CONFIG_MACRO_DIR/g' configure.ac > configure2.ac
-            mv -f configure2.ac configure.ac
-        fi
-    fi
-fi
-
 mkdir -p m4 || exit $?
 autoreconf --install || exit $?
 
