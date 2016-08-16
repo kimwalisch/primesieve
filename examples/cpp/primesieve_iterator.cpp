d6755631278b1f07c217dfd0f6e0030c6e1864cb
Kim Walisch <kim.walisch@gmail.com>
2016-08-16 11:42:31 +0200

    Simplify code

--- examples/cpp/primesieve_iterator.cpp
+++ examples/cpp/primesieve_iterator.cpp
@@ -1,5 +1,5 @@
 /// @example primesieve_iterator.cpp
-/// Iterate over primes using a primesieve::iterator object.
+/// Iterate over primes using primesieve::iterator.
 
 #include <primesieve.hpp>
 #include <iostream>
@@ -7,11 +7,11 @@
 int main()
 {
   primesieve::iterator it;
+  uint64_t prime = it.next_prime();
   uint64_t sum = 0;
-  uint64_t prime;
 
-  // iterate over primes below 10^10
-  while ((prime = it.next_prime()) < 10000000000ull)
+  // iterate over the primes below 10^10
+  for (; prime < 10000000000ull; prime = it.next_prime())
     sum += prime;
 
   std::cout << "Sum of the primes below 10^10 = " << sum << std::endl;
