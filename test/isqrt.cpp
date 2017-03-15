7b6843fd50c0e95a865c30442b36f10b9588d11c
Kim Walisch <kim.walisch@gmail.com>
2017-03-15 17:20:27 +0100

    Test integer square root function

---
+++ test/isqrt.cpp
@@ -0,0 +1,84 @@
+///
+/// @file   isqrt.cpp
+/// @brief  Test integer square root function.
+///
+/// Copyright (C) 2017 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
+#include <primesieve/pmath.hpp>
+
+#include <stdint.h>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+using namespace primesieve;
+
+void check(bool OK)
+{
+  cout << "   " << (OK ? "OK" : "ERROR") << "\n";
+  if (!OK)
+    exit(1);
+}
+
+int main()
+{
+  uint64_t n;
+  uint64_t res1;
+  double res2;
+
+  for (n = 0; n < 1000; n++)
+  {
+    res1 = isqrt(n);
+    res2 = sqrt((double) n);
+    cout << "isqrt(" << n << ") = " << res1;
+    check(res1 == (uint64_t) res2);
+  }
+
+  n = (1ull << 32) - 1;
+  res1 = isqrt(n);
+  res2 = sqrt((double) n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == (uint64_t) res2);
+
+  n = 1ull << 32;
+  res1 = isqrt(n);
+  res2 = sqrt((double) n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == (uint64_t) res2);
+
+  n = (1ull << 32) + 1;
+  res1 = isqrt(n);
+  res2 = sqrt((double) n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == (uint64_t) res2);
+
+  n = 9223372037000250000ull - 1;
+  res1 = isqrt(n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == 3037000499ull);
+
+  n = 9223372037000250000ull;
+  res1 = isqrt(n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == 3037000500ull);
+
+  n = 9223372037000250000ull + 1;
+  res1 = isqrt(n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == 3037000500ull);
+
+  n = 18446744073709551615ull;
+  res1 = isqrt(n);
+  cout << "isqrt(" << n << ") = " << res1;
+  check(res1 == 4294967295ull);
+
+  cout << endl;
+  cout << "All tests passed successfully!" << endl;
+
+  return 0;
+}
