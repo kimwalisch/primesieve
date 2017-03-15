6bfec19ab59773923d6ad82a540cea5b290571dc
Kim Walisch <kim.walisch@gmail.com>
2017-03-15 20:25:24 +0100

    Refactor

--- include/primesieve/pmath.hpp
+++ include/primesieve/pmath.hpp
@@ -35,14 +35,12 @@ inline T isquare(T x)
   return x * x;
 }
 
-/// Check if an integer is a power of 2
 template <typename T>
 inline bool isPowerOf2(T x)
 {
-  return (x != 0 && (x & (x - 1)) == 0);
+  return x != 0 && (x & (x - 1)) == 0;
 }
 
-/// Round down to the next power of 2
 template <typename T>
 inline T floorPowerOf2(T x)
 {
@@ -51,13 +49,13 @@ inline T floorPowerOf2(T x)
   return x - (x >> 1);
 }
 
-/// Fast and protable integer log2 function
 template <typename T>
 inline T ilog2(T x)
 {
-  const T bits = numberOfBits(x);
-  const T one = 1;
+  T bits = numberOfBits(x);
+  T one = 1;
   T log2 = 0;
+
   for (T i = bits / 2; i != 0; i /= 2)
   {
     if (x >= (one << i))
@@ -66,22 +64,22 @@ inline T ilog2(T x)
       log2 += i;
     }
   }
+
   return log2;
 }
 
 /// Integer square root, Newton's method
+/// @see book "Hacker's Delight"
+///
 template <typename T>
 inline T isqrt(T x)
 {
   if (x <= 1)
     return x;
 
-  const T bits = numberOfBits(x);
-  const T one = 1;
-
-  // s = bits / 2 - nlz(x - 1) / 2
-  // nlz(x) = bits - 1 - ilog2(x)
-  T s = bits / 2 - (bits - 1) / 2 + ilog2(x - 1) / 2;
+  T bits = numberOfBits(x);
+  T one = 1;
+  T s = (1 + ilog2(x - 1)) / 2;
 
   // first guess: least power of 2 >= sqrt(x)
   T g0 = one << s;
@@ -92,19 +90,11 @@ inline T isqrt(T x)
     g0 = g1;
     g1 = (g0 + (x / g0)) >> 1;
   }
+
   return g0;
 }
 
-template <typename T1, typename T2, typename T3>
-inline T2 inBetween(T1 min, T2 x, T3 max)
-{
-  if (x < (T2) min) return (T2) min;
-  if (x > (T2) max) return (T2) max;
-
-  return x;
-}
-
-/// Returns a+b or 2^64-1 if the result overflows
+/// Returns UINT64_MAX if x + y >= 2^64-1
 inline uint64_t add_overflow_safe(uint64_t x, uint64_t y)
 {
   if (x >= std::numeric_limits<uint64_t>::max() - y)
@@ -113,10 +103,19 @@ inline uint64_t add_overflow_safe(uint64_t x, uint64_t y)
   return x + y;
 }
 
-/// Returns a-b or 0 if the result underflows
+/// Returns 0 if x - y <= 0
 inline uint64_t sub_underflow_safe(uint64_t x, uint64_t y)
 {
   return (x > y) ? x - y : 0;
+}
+
+template <typename A, typename B, typename C>
+inline B inBetween(A min, B x, C max)
+{
+  if (x < (B) min) return (B) min;
+  if (x > (B) max) return (B) max;
+
+  return x;
 }
 
 /// Get an approximation of the maximum prime gap near n
