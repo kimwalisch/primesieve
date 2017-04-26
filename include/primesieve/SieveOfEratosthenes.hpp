4128145772f8a90671cda0d071caaa9d5bebeaad
Kim Walisch <kim.walisch@gmail.com>
2017-04-26 22:49:32 +0200

    Update comments

--- include/primesieve/SieveOfEratosthenes.hpp
+++ include/primesieve/SieveOfEratosthenes.hpp
@@ -27,7 +27,7 @@ class PreSieve;
 /// segmented sieve of Eratosthenes. It uses a bit array for sieving,
 /// the bit array uses 8 flags for 30 numbers. SieveOfEratosthenes
 /// uses three different sieve of Eratosthenes algorithms optimized
-/// for small, medium and big sieving primes to cross-off multiples.
+/// for small, medium and big sieving primes to cross-off multiples
 ///
 class SieveOfEratosthenes
 {
@@ -76,12 +76,12 @@ private:
   void sieveSegment();
 };
 
-/// Reconstruct the prime number corresponding to the first set
-/// bit of the `bits' parameter and unset that bit.
+/// Reconstruct the prime number corresponding to
+/// the first set bit and unset that bit
 ///
 inline uint64_t SieveOfEratosthenes::getNextPrime(uint64_t* bits, uint64_t base)
 {
-  // calculate bitValues_[ bitScanForward(*bits) ]
+  // calculate bitValues_[bitScanForward(*bits)]
   // using a custom De Bruijn bitscan
   uint64_t debruijn64 = 0x3F08A4C6ACB9DBDull;
   uint64_t mask = *bits - 1;
@@ -92,13 +92,13 @@ inline uint64_t SieveOfEratosthenes::getNextPrime(uint64_t* bits, uint64_t base)
 }
 
 /// This method is called consecutively for all
-/// sieving primes up to sqrt(stop).
+/// sieving primes up to sqrt(stop)
 ///
 inline void SieveOfEratosthenes::addSievingPrime(uint_t prime)
 {
   uint64_t square = isquare<uint64_t>(prime);
 
-  // This loop is executed once all primes <= sqrt(segmentHigh_)
+  // This loop is executed once all primes <= sqrt(segmentHigh)
   // required to sieve the next segment have been
   // added to the erat* objects further down
   while (segmentHigh_ < square)
