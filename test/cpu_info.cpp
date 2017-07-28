08eb5b2a4c941c1bcc2ea9e0cba0dbfcdd21d927
Kim Walisch <kim.walisch@gmail.com>
2017-07-28 11:13:22 +0200

    Add cache size checks

--- test/cpu_info.cpp
+++ test/cpu_info.cpp
@@ -16,9 +16,35 @@ using namespace primesieve;
 
 int main()
 {
+  int exit_code = 0;
+
   cout << "L1 cache size: " << cpuInfo.l1CacheSize() / 1024 << " KB" << endl;
   cout << "L2 cache size: " << cpuInfo.l2CacheSize() / 1024 << " KB" << endl;
   cout << "L3 cache size: " << cpuInfo.l3CacheSize() / 1024 << " KB" << endl;
 
-  return 0;
+  if (cpuInfo.l1CacheSize() > 0 &&
+      (cpuInfo.l1CacheSize() < 1024 ||
+       cpuInfo.l1CacheSize() > (1ull << 40)))
+  {
+    cerr << "Error: L1 cache size does not look right!" << endl;
+    exit_code = 1;
+  }
+
+  if (cpuInfo.l2CacheSize() > 0 &&
+      (cpuInfo.l2CacheSize() < 1024 ||
+       cpuInfo.l2CacheSize() > (1ull << 40)))
+  {
+    cerr << "Error: L2 cache size does not look right!" << endl;
+    exit_code = 1;
+  }
+
+  if (cpuInfo.l3CacheSize() > 0 &&
+      (cpuInfo.l3CacheSize() < 1024 ||
+       cpuInfo.l3CacheSize() > (1ull << 40)))
+  {
+    cerr << "Error: L3 cache size does not look right!" << endl;
+    exit_code = 1;
+  }
+
+  return exit_code;
 }
