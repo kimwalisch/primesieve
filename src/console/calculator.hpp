5c7b588a43db4de0af59912f343ade5259dfa7a6
Kim Walisch <kim.walisch@gmail.com>
2017-08-11 22:13:12 +0200

    Silence GCC 7 compiler warning
    Silence GCC 7 -Wimplicit-fallthrough warning

--- src/console/calculator.hpp
+++ src/console/calculator.hpp
@@ -363,10 +363,10 @@ private:
     switch (getCharacter())
     {
       case '0': if (isHex())
-                {
                   val = parseHex();
-                  break;
-                }
+                else
+                  val = parseDecimal();
+                break;
       case '1': case '2': case '3': case '4': case '5':
       case '6': case '7': case '8': case '9':
                 val = parseDecimal();
