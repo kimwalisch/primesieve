563b324c5ea9fb8ac9a59fa7a58e10765f4fa8d7
Kim Walisch <kim.walisch@gmail.com>
2017-11-11 20:10:33 +0100

    Increase version to 6.3

--- include/primesieve.h
+++ include/primesieve.h
@@ -12,9 +12,9 @@
 #ifndef PRIMESIEVE_H
 #define PRIMESIEVE_H
 
-#define PRIMESIEVE_VERSION "6.2"
+#define PRIMESIEVE_VERSION "6.3"
 #define PRIMESIEVE_VERSION_MAJOR 6
-#define PRIMESIEVE_VERSION_MINOR 2
+#define PRIMESIEVE_VERSION_MINOR 3
 
 #include <primesieve/iterator.h>
 
