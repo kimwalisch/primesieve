136bf93a23c2b194e662745cebbb0783255b867b
Kim Walisch <kim.walisch@gmail.com>
2018-02-24 20:04:23 +0100

    Update google analytics code

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -98,10 +98,12 @@ $(document).ready(function()
   $('a.download_targz').attr('href', download_url + 'v' + primesieve_version + '/primesieve-' + primesieve_version + '.tar.gz');
 });
 
+<!-- Google Analytics -->
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-47069346-1', 'primesieve.org');
 ga('send', 'pageview');
+<!-- End Google Analytics -->
