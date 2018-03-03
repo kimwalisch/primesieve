9fbfd022e591c995ad4721295173a4ecf8b511e0
Kim Walisch <kim.walisch@gmail.com>
2018-03-03 08:47:09 +0100

    Move GitHub analytics out of custom.js
    to speed up website loading.

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -105,15 +105,3 @@ $(document).ready(function()
     $('a.download_button span').html(download_button_text);
   }
 });
-
-// Google Analytics
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-47069346-1', 'primesieve.org');
-ga('send', 'pageview');
-
-// End Google Analytics
