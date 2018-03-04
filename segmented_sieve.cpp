01f5b06fc2780d4238160c880a5dd813915d4a0c
Kim Walisch <kim.walisch@gmail.com>
2018-03-04 19:09:00 +0100

    Improve segmented sieve

--- segmented_sieve.cpp
+++ segmented_sieve.cpp
@@ -14,7 +14,7 @@
 #include <stdint.h>
 
 /// Set your CPU's L1 data cache size (in bytes) here
-const int L1D_CACHE_SIZE = 32768;
+const int64_t L1D_CACHE_SIZE = 32768;
 
 /// Generate primes using the segmented sieve of Eratosthenes.
 /// This algorithm uses O(n log log n) operations and O(sqrt(n)) space.
@@ -22,49 +22,51 @@ const int L1D_CACHE_SIZE = 32768;
 ///
 void segmented_sieve(int64_t limit)
 {
-  int sqrt = (int) std::sqrt(limit);
-  int segment_size = std::max(sqrt, L1D_CACHE_SIZE);
-
+  int64_t sqrt = (int64_t) std::sqrt(limit);
+  int64_t segment_size = std::max(sqrt, L1D_CACHE_SIZE);
   int64_t count = (limit < 2) ? 0 : 1;
-  int64_t s = 3;
+
+  // we sieve primes >= 3
+  int64_t i = 3;
   int64_t n = 3;
+  int64_t s = 3;
 
-  // generate small primes <= sqrt
-  std::vector<char> is_prime(sqrt + 1, 1);
-  for (int i = 2; i * i <= sqrt; i++)
-    if (is_prime[i])
-      for (int j = i * i; j <= sqrt; j += i)
-        is_prime[j] = 0;
-
-  // vector used for sieving
   std::vector<char> sieve(segment_size);
-  std::vector<int> primes;
-  std::vector<int> next;
+  std::vector<char> is_prime(sqrt + 1, true);
+  std::vector<int64_t> primes;
+  std::vector<int64_t> indexes;
 
   for (int64_t low = 0; low <= limit; low += segment_size)
   {
-    std::fill(sieve.begin(), sieve.end(), 1);
+    std::fill(sieve.begin(), sieve.end(), true);
 
-    // current segment = interval [low, high]
+    // current segment = [low, high]
     int64_t high = std::min(low + segment_size - 1, limit);
+    int64_t sqrt_high = (int64_t) std::sqrt(high);
 
-    // add new sieving primes <= sqrt(high)
-    for (; s * s <= high; s += 2)
+    // generate sieving primes using simple sieve of Eratosthenes
+    for (; i <= sqrt_high; i += 2)
+      if (is_prime[i])
+        for (int64_t j = i * i; j <= sqrt; j += i)
+          is_prime[j] = false;
+
+    // initialize sieving primes for segmented sieve
+    for (; s <= sqrt_high; s += 2)
     {
       if (is_prime[s])
       {
-        primes.push_back((int) s);
-          next.push_back((int)(s * s - low));
+         primes.push_back(s);
+        indexes.push_back(s * s - low);
       }
     }
-    
-    // sieve the current segment
+
+    // segmented sieve
     for (std::size_t i = 0; i < primes.size(); i++)
     {
-      int j = next[i];
-      for (int k = primes[i] * 2; j < segment_size; j += k)
-        sieve[j] = 0;
-      next[i] = j - segment_size;
+      int64_t j = indexes[i];
+      for (int64_t k = primes[i] * 2; j < segment_size; j += k)
+        sieve[j] = false;
+      indexes[i] = j - segment_size;
     }
 
     for (; n <= high; n += 2)
