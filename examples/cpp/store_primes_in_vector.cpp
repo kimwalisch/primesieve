623fe28a31f418721c1d88b64664d6d557d9a5dd
Kim Walisch <kim.walisch@statec.etat.lu>
2018-04-23 15:43:06 +0200

    Add more examples

--- examples/cpp/store_primes_in_vector.cpp
+++ examples/cpp/store_primes_in_vector.cpp
@@ -8,13 +8,23 @@ int main()
 {
   std::vector<int> primes;
 
-  // Store the primes <= 1000
+  // Store primes <= 1000
   primesieve::generate_primes(1000, &primes);
 
   primes.clear();
 
-  // Store the first 1000 primes
+  // Store primes inside [1000, 2000]
+  primesieve::generate_primes(1000, 2000, &primes);
+
+  primes.clear();
+
+  // Store first 1000 primes
   primesieve::generate_n_primes(1000, &primes);
+
+  primes.clear();
+
+  // Store first 10 primes >= 1000
+  primesieve::generate_n_primes(10, 1000, &primes);
 
   return 0;
 }
