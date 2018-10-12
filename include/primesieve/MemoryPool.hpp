e496dc50cc7a80377bb71dfb36319bd22c0dfd8b
kimwalisch <kim.walisch@gmail.com>
2018-10-13 00:25:16 +0200

    Fix MSVC performance regression
    MSVC runs up to 20% faster if the code is inlined

--- include/primesieve/MemoryPool.hpp
+++ include/primesieve/MemoryPool.hpp
@@ -1,5 +1,14 @@
 ///
 /// @file  MemoryPool.hpp
+///        EratMedium and EratBig may use millions of buckets for
+///        storing the sieving primes that are required to cross off
+///        multiples. As many memory allocations/deallocations are
+///        bad for performance the MemoryPool initially allocates a
+///        large number of buckets (using a single memory allocation)
+///        and puts the buckets into its stock. The MemoryPool can
+///        then serve buckets to EratMedium and EratBig without
+///        doing any memory allocation as long as the MemoryPool's
+///        stock is not empty.
 ///
 /// Copyright (C) 2018 Kim Walisch, <kim.walisch@gmail.com>
 ///
@@ -14,20 +23,59 @@
 #include "config.hpp"
 
 #include <stdint.h>
-#include <vector>
+#include <algorithm>
 #include <memory>
+#include <vector>
 
 namespace primesieve {
 
 class MemoryPool
 {
 public:
-  void addBucket(Bucket*& dest);
-  void freeBucket(Bucket* b);
+  void addBucket(Bucket*& list)
+  {
+    // allocate new buckets
+    if (!stock_)
+    {
+      if (memory_.empty())
+        memory_.reserve(128);
+
+      Bucket* buckets = new Bucket[count_];
+      memory_.emplace_back(std::unique_ptr<Bucket[]>(buckets));
+
+      // init buckets
+      for (uint64_t i = 0; i < count_; i++)
+      {
+        Bucket* next = nullptr;
+        if (i + 1 < count_)
+          next = &buckets[i + 1];
+
+        buckets[i].reset();
+        buckets[i].setNext(next);
+      }
+
+      // put new buckets into stock
+      stock_ = buckets;
+      count_ += count_ / 8;
+      count_ = std::min(count_, maxCount_);
+    }
+
+    // get first bucket
+    Bucket& bucket = *stock_;
+    stock_ = stock_->next();
+    bucket.setNext(list);
+    list = &bucket;
+  }
+
+  void freeBucket(Bucket* b)
+  {
+    Bucket& bucket = *b;
+    bucket.reset();
+    bucket.setNext(stock_);
+    stock_ = &bucket;
+  }
+
 private:
-  void allocateBuckets();
-  void increaseAllocCount();
-  Bucket* getBucket();
   /// EratMedium::lists_.size() * 2 = 128
   /// so EratMedium will require only 1 allocation
   uint64_t count_ = 128;
