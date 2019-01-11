d8a5437c103df02b46d7d7888a3b80266e6b69ee
kimwalisch <kim.walisch@gmail.com>
2019-01-11 22:34:42 +0100

    Remove useless macro

--- src/CpuInfo.cpp
+++ src/CpuInfo.cpp
@@ -176,8 +176,7 @@ namespace primesieve {
 void CpuInfo::init()
 {
 // Windows 7 (2009) or later
-#if defined(_WIN32_WINNT) && \
-    _WIN32_WINNT >= 0x0601
+#if _WIN32_WINNT >= 0x0601
 
   using LPFN_GLPIEX = decltype(&GetLogicalProcessorInformationEx);
 
