ba144d148d043498071205d496def0be6d93d96f
Kim Walisch <kim.walisch@gmail.com>
2022-01-06 18:20:42 +0100

    Refactor

--- src/PreSieve.cpp
+++ src/PreSieve.cpp
@@ -253,9 +253,9 @@ void PreSieve::preSieveSmall(uint8_t* sieve,
                              uint64_t sieveSize,
                              uint64_t segmentLow) const
 {
-  uint64_t primeProduct = buffer_7_11_13.size() * 30;
-  uint64_t i = (segmentLow % primeProduct) / 30;
   uint64_t size = buffer_7_11_13.size();
+  uint64_t primeProduct = size * 30;
+  uint64_t i = (segmentLow % primeProduct) / 30;
   uint64_t sizeLeft = size - i;
   auto buffer = buffer_7_11_13.data();
 
