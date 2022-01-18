bb3e5cf5fa28d3e7a92b1e151d5a98337ab8341b
Kim Walisch <kim.walisch@gmail.com>
2022-01-18 10:30:03 +0100

    Convert to benchmark program

--- examples/cpp/primesieve_iterator.cpp
+++ examples/cpp/primesieve_iterator.cpp
@@ -2,26 +2,31 @@
 /// Iterate over primes using primesieve::iterator.
 
 #include <primesieve.hpp>
+#include <cstdlib>
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
   primesieve::iterator it;
+  uint64_t limit = 10000000000ull;
   uint64_t prime = it.next_prime();
   uint64_t sum = 0;
 
+  if (argc > 1)
+    limit = std::atol(argv[1]);
+
   // iterate over the primes below 10^9
-  for (; prime < 1000000000ull; prime = it.next_prime())
+  for (; prime < limit; prime = it.next_prime())
     sum += prime;
 
-  std::cout << "Sum of the primes below 10^9 = " << sum << std::endl;
+  std::cout << "Sum of primes < " << limit << " = " << sum << std::endl;
 
-  // generate primes > 1000
-  it.skipto(1000);
-  prime = it.next_prime();
-
-  for (; prime < 1100; prime = it.next_prime())
-    std::cout << prime << std::endl;
+  // Note that since sum is a 64-bit variable the result
+  // will be incorrect (due to integer overflow) if
+  // limit > 10^10. However we do allow limits > 10^10
+  // since this is useful for benchmarking.
+  if (limit > 10000000000ull)
+    std::cerr << "Warning: sum is likely incorrect due to 64-bit integer overflow!" << std::endl;
 
   return 0;
 }
