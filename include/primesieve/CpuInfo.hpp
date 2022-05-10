4178dc745095ca1b23dc46b9731cdcb1645a23f1
Kim Walisch <kim.walisch@gmail.com>
2022-05-10 10:23:25 +0200

    Update comment

--- include/primesieve/CpuInfo.hpp
+++ include/primesieve/CpuInfo.hpp
@@ -42,7 +42,7 @@ public:
   {
     // By inlining and hardcoding 'return true' here if the compiler
     // defines the AVX512 macros, the AVX512 runtime check in
-    // iterator.cpp will be removed if the user compiles with
+    // PrimeGenerator.hpp will be removed if the user compiles with
     // -march=native on a CPU that supports AVX512.
     #if defined(__AVX512F__) && \
         defined(__AVX512VBMI__) && \
