3200cdec1ca3aab1b02b7544ce0d0e51f228f407
Kim Walisch <kim.walisch@gmail.com>
2023-05-06 14:22:20 +0200

    Release build script

---
+++ scripts/build_mingw64_clang.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Usage: scripts/build_mingw64_clang.sh ARCH
+# @ARCH: either x64 or arm64
+# Builds a primesieve release binary that is statically linked
+# and ready for distribution.
+
+# Prerequisites:
+# 1) Install MSYS2 x64 (or arm64 if available)
+# 2) Open C:/msys64/clangarm64.exe
+# 3) pacman -Syu (exit then run it again)
+# 4) pacman -S mingw-w64-clang-aarch64-clang mingw-w64-clang-aarch64-openmp make cmake git zip unzip
+# 5) cd primesieve
+# 6) scripts/build_mingw64_clang.sh x64
+
+if [ $# -ne 1 ]
+then
+    echo "Usage examples:"
+    echo "$ scripts/build_mingw64_clang.sh x64"
+    echo "Build primesieve for the x64 CPU architecture."
+    echo "$ scripts/build_mingw64_clang.sh arm64"
+    echo "Build primesieve for the arm64 CPU architecture."
+    exit 1
+fi
+
+arch=$1
+
+# Exit if any error occurs
+set -e
+
+rm -rf build*
+
+####################################################################
+
+FULL_DATE=$(date +'%B %d, %Y')
+YEAR=$(date +'%Y')
+
+cd include
+VERSION=$(grep "primesieve_VERSION " primesieve.hpp | cut -f2 -d'"')
+cd ..
+
+####################################################################
+
+handle_error() {
+    echo ""
+    echo "Error: $1"
+    exit 1
+}
+
+####################################################################
+
+# The repo must no have any uncommited changes as we
+# switch to another branch during the script.
+git diff --exit-code > /dev/null || handle_error "repo must not have any uncommitted changes"
+
+# Build primesieve binary ##########################################
+
+git pull
+mkdir build-release
+cd build-release
+
+clang++ -static -O3 -DNDEBUG -D_WIN32_WINNT=0x0A00 -Wall -Wextra -pedantic -I ../include ../src/*.cpp ../src/app/*.cpp -o primesieve.exe
+strip primesieve.exe
+
+# Create a release zip archive
+wget https://github.com/kimwalisch/primesieve/releases/download/v6.1/primesieve-11.0-win-$arch.zip
+unzip primesieve-11.0-win-$arch.zip -d primesieve-$VERSION-win-$arch
+rm primesieve-11.0-win-$arch.zip
+
+echo ""
+echo ""
+echo "Old file size: $(ls -l --block-size=K primesieve-$VERSION-win-$arch/primesieve.exe)"
+echo "New file size: $(ls -l --block-size=K primesieve.exe)"
+echo ""
+echo ""
+
+mv -f primesieve.exe primesieve-$VERSION-win-$arch
+cd primesieve-$VERSION-win-$arch
+sed -i "1 s/.*/primesieve $VERSION/" README.txt
+sed -i "2 s/.*/$FULL_DATE/" README.txt
+sed -i "3 s/.*/Copyright \(c\) 2010 - $YEAR, Kim Walisch\./" COPYING
+
+# Verify sed has worked correctly
+[ "$(sed -n '1p' < README.txt)" = "primesieve $VERSION" ] || handle_error "failed updating README.txt"
+[ "$(sed -n '2p' < README.txt)" = "$FULL_DATE" ] || handle_error "failed updating README.txt"
+[ "$(sed -n '3p' < COPYING)" = "Copyright (c) 2010 - $YEAR, Kim Walisch." ] || handle_error "failed updating COPYING"
+
+zip primesieve-$VERSION-win-$arch.zip primesieve.exe README.txt COPYING
+cp primesieve-$VERSION-win-$arch.zip ..
+
+./primesieve --test
+echo ""
+echo ""
+./primesieve 1e11 -s
+
+cd ..
+
+####################################################################
+
+echo ""
+echo "Release binary built successfully!"
