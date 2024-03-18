b1fcc5961a4c363897562888e0c0aa1b12ec5c3b
Kim Walisch <kim.walisch@gmail.com>
2024-03-18 16:30:59 +0100

    Add more tests

--- test/Riemann_R.cpp
+++ test/Riemann_R.cpp
@@ -20,6 +20,22 @@
 using std::size_t;
 using namespace primesieve;
 
+/// Generated using Mathematica:
+/// Table[IntegerPart[RiemannR[k]], {k, 0, 99}]
+Array<uint64_t, 100> RiemannR_tiny =
+{
+  0, 1, 1, 2, 2, 2, 3, 3, 3, 4,
+  4, 4, 5, 5, 5, 6, 6, 6, 6, 7,
+  7, 7, 8, 8, 8, 8, 9, 9, 9, 9,
+  10, 10, 10, 10, 11, 11, 11, 11, 12, 12,
+  12, 12, 13, 13, 13, 13, 14, 14, 14, 14,
+  14, 15, 15, 15, 15, 16, 16, 16, 16, 17,
+  17, 17, 17, 17, 18, 18, 18, 18, 18, 19,
+  19, 19, 19, 20, 20, 20, 20, 20, 21, 21,
+  21, 21, 21, 22, 22, 22, 22, 23, 23, 23,
+  23, 23, 24, 24, 24, 24, 24, 25, 25, 25
+};
+
 Array<uint64_t, 14> RiemannR_table =
 {
                      4, // RiemannR(10^1)
@@ -47,6 +63,12 @@ void check(bool OK)
 
 int main()
 {
+  for (size_t x = 0; x < RiemannR_tiny.size(); x++)
+  {
+    std::cout << "RiemannR(" << x << ") = " << (uint64_t) RiemannR((long double) x);
+    check((uint64_t) RiemannR((long double) x) == RiemannR_tiny[x]);
+  }
+
   uint64_t x = 1;
   for (size_t i = 0; i < RiemannR_table.size(); i++)
   {
