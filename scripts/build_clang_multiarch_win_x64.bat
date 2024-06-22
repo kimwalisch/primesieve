919a9ad0ebd9c91eeb9745edbfda9fee5832574d
Kim Walisch <kim.walisch@gmail.com>
2024-06-22 16:45:15 +0200

    Add -DENABLE_MULTIARCH_x86_POPCNT

--- scripts/build_clang_multiarch_win_x64.bat
+++ scripts/build_clang_multiarch_win_x64.bat
@@ -1,1 +1,1 @@
-clang++ -I../include -O3 -DNDEBUG -DENABLE_MULTIARCH_AVX512_VBMI2 ../src/*.cpp ../src/app/*.cpp -o primesieve.exe "C:\Program Files\LLVM\lib\clang\18\lib\windows\clang_rt.builtins-x86_64.lib"
+clang++ -I../include -O3 -DNDEBUG -DENABLE_MULTIARCH_x86_POPCNT -DENABLE_MULTIARCH_AVX512_VBMI2 ../src/*.cpp ../src/app/*.cpp -o primesieve.exe "C:\Program Files\LLVM\lib\clang\18\lib\windows\clang_rt.builtins-x86_64.lib"
