f86c691203d36d66e0d65f2c47764bb6212b7a5c
kimwalisch <kim.walisch@gmail.com>
2025-03-01 10:04:57 +0100

    Get rid of <asm/hwcap.h> dependency

--- src/arch/arm/sve.cpp
+++ src/arch/arm/sve.cpp
@@ -47,8 +47,16 @@ bool has_arm_sve()
        __has_include(<sys/auxv.h>)
 
 #include <sys/auxv.h>
-#include <asm/hwcap.h>
 #include <errno.h>
+
+// The Linux kernel header <asm/hwcap.h> is not installed by
+// default on some Linux distros. Hence we define HWCAP_SVE
+// for ARM64 CPUs to get rid of the <asm/hwcap.h> dependency.
+#if defined(__aarch64__)
+  #define HWCAP_SVE (1 << 22)
+#else
+  #include <asm/hwcap.h>
+#endif
 
 namespace primesieve {
 
@@ -83,7 +91,7 @@ bool has_arm_sve()
 {
   // Since __builtin_cpu_init() and __builtin_cpu_supports() are
   // currently (2025) not yet supported for ARM64 CPUs by both
-  // GCC and Clang, we only use them as a fallback option if
+  // GCC and Clang, we only try them as a fallback option if
   // none of the other more reliable methods work.
   __builtin_cpu_init();
   if (__builtin_cpu_supports("sve"))
