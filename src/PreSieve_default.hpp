09210f53ec7e0e3044db913f34eae535d499c9b0
kimwalisch <kim.walisch@gmail.com>
2025-04-06 20:23:00 +0200

    Add comment

--- src/PreSieve_default.hpp
+++ src/PreSieve_default.hpp
@@ -1,5 +1,21 @@
 ///
-/// @file PreSieve_default.hpp
+/// @file   PreSieve_default.hpp
+/// @brief  The presieve*_default() functions are important for
+///         performance and therefore it is important that these
+///         functions are auto-vectorized by the compiler. If these
+///         functions are not auto-vectorized primesieve's
+///         performance will deteriorate by up to 30%.
+///
+///         Clang vectorizes presieve*_default() with -O2 and -O3,
+///         which is great. GCC only vectorizes presieve*_default()
+///         with -O3 but not with -O2. GCC -O2 uses the very-cheap
+///         cost model which prevents our presieve*_default()
+///         functions from getting auto vectorized. But compiling with
+///         "gcc -O2 -ftree-vectorize -fvect-cost-model=dynamic"
+///         fixes this issue. Therefore auto_vectorization.cmake
+///         enables the use of the -ftree-vectorize and
+///         -fvect-cost-model=dynamic compiler options if they are
+///         supported by the compiler.
 ///
 /// Copyright (C) 2025 Kim Walisch, <kim.walisch@gmail.com>
 /// Copyright (C) 2022 @zielaj, https://github.com/zielaj
