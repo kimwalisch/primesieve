efb837327ab18d0368f4c0721813bc1095f146ad
Kim Walisch <kim.walisch@gmail.com>
2025-04-08 09:41:54 +0200

    Refactor

--- include/primesieve/littleendian_cast.hpp
+++ include/primesieve/littleendian_cast.hpp
@@ -3,7 +3,7 @@
 /// @brief  Cast bytes in ascending address order on both little and
 ///         big endian CPUs.
 ///
-/// Copyright (C) 2020 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2025 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
@@ -12,55 +12,38 @@
 #ifndef LITTLEENDIAN_CAST_HPP
 #define LITTLEENDIAN_CAST_HPP
 
+#if defined(__BYTE_ORDER__) && \
+    defined(__ORDER_BIG_ENDIAN__) && \
+    __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+
 #include <stdint.h>
+#include <cstring>
 
 namespace {
-
-/// http://c-faq.com/misc/endiantest.html
-inline bool is_littleendian()
-{
-  union {
-    int word;
-    char c[sizeof(int)];
-  } x;
-  x.word = 1;
-  return (x.c[0] == 1);
-}
-
-/// Recursively sum bytes using template metaprogramming.
-/// e.g. littleendian_cast<int32_t>(array) =
-/// return (array[0] <<  0) +
-///        (array[1] <<  8) +
-///        (array[2] << 16) +
-///        (array[3] << 24);
-///
-template <typename T, int INDEX, int STOP>
-struct littleendian_cast_helper
-{
-  static T sum(const uint8_t* array, T n)
-  {
-    n += static_cast<T>(array[INDEX]) << (INDEX * 8);
-    return littleendian_cast_helper<T, INDEX + 1, STOP - 1>::sum(array, n);
-  }
-};
-
-template <typename T, int INDEX>
-struct littleendian_cast_helper<T, INDEX, 0>
-{
-  static T sum(const uint8_t*, T n)
-  {
-    return n;
-  }
-};
 
 template <typename T>
 inline T littleendian_cast(const uint8_t* array)
 {
-  if (is_littleendian())
-    return *reinterpret_cast<const T*>(array);
-  return littleendian_cast_helper<T, 0, sizeof(T)>::sum(array, 0);
+  T result;
+  std::memcpy(&result, array, sizeof(T));
+  return result;
 }
 
 } // namespace
+
+#else
+
+#include <macros.hpp>
+#include <stdint.h>
+
+template <typename T>
+inline T littleendian_cast(const uint8_t* array)
+{
+  // Disallow unaligned memory accesses
+  ASSERT(uintptr_t(array) % sizeof(T) == 0);
+  return *reinterpret_cast<const T*>(array);
+}
+
+#endif
 
 #endif
