2e7d113dbbd7209a750c3bc328df3681d3328882
Kim Walisch <kim.walisch@gmail.com>
2025-04-22 09:29:02 +0200

    Add missing include guards

--- include/primesieve/RiemannR.hpp
+++ include/primesieve/RiemannR.hpp
@@ -1,11 +1,14 @@
 ///
 /// @file  RiemannR.hpp
 ///
-/// Copyright (C) 2024 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2025 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
 ///
+
+#ifndef RIEMANNR_HPP
+#define RIEMANNR_HPP
 
 #include <stdint.h>
 
@@ -18,3 +21,5 @@ uint64_t primePiApprox(uint64_t x);
 uint64_t nthPrimeApprox(uint64_t n);
 
 } // namespace
+
+#endif
