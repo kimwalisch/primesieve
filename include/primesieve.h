12a7fc5dd36602e1e416366fcc4d087d293d3f25
kimwalisch <kim.walisch@gmail.com>
2025-05-01 09:48:05 +0200

    Update version to 12.9

--- include/primesieve.h
+++ include/primesieve.h
@@ -15,9 +15,9 @@
 #ifndef PRIMESIEVE_H
 #define PRIMESIEVE_H
 
-#define PRIMESIEVE_VERSION "12.8"
+#define PRIMESIEVE_VERSION "12.9"
 #define PRIMESIEVE_VERSION_MAJOR 12
-#define PRIMESIEVE_VERSION_MINOR 8
+#define PRIMESIEVE_VERSION_MINOR 9
 
 #include <primesieve/iterator.h>
 
