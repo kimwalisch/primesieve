21a938b869d622fc98370e9b132ca076777d3ca6
Kim Walisch <kim.walisch@gmail.com>
2025-05-02 18:26:04 +0200

    Revert

--- src/CountPrintPrimes.hpp
+++ src/CountPrintPrimes.hpp
@@ -12,6 +12,7 @@
 
 #include "Erat.hpp"
 #include "MemoryPool.hpp"
+#include "PrimeSieveClass.hpp"
 
 #include <primesieve/macros.hpp>
 #include <primesieve/Vector.hpp>
@@ -21,7 +22,6 @@
 namespace primesieve {
 
 class Store;
-class PrimeSieve;
 
 /// After a segment has been sieved CountPrintPrimes is
 /// used to reconstruct primes and prime k-tuplets from
