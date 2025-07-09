02505da1a067816849a3530f115b6dd5dc88c1c1
kimwalisch <kim.walisch@gmail.com>
2025-07-09 20:34:12 +0200

    Increase version to 12.10

--- include/primesieve.h
+++ include/primesieve.h
@@ -15,9 +15,9 @@
 #ifndef PRIMESIEVE_H
 #define PRIMESIEVE_H
 
-#define PRIMESIEVE_VERSION "12.9"
+#define PRIMESIEVE_VERSION "12.10"
 #define PRIMESIEVE_VERSION_MAJOR 12
-#define PRIMESIEVE_VERSION_MINOR 9
+#define PRIMESIEVE_VERSION_MINOR 10
 
 #include <primesieve/iterator.h>
 
