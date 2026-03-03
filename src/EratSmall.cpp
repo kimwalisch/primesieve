1eb4a402ca0207a2f026ec3b1054e8d268a1b776
Kim Walisch <kim.walisch@gmail.com>
2026-03-03 16:00:54 +0100

    Update

--- src/EratSmall.cpp
+++ src/EratSmall.cpp
@@ -29,11 +29,39 @@ namespace primesieve {
 
 namespace {
 
-template <uint8_t BASE,
+const uint8_t wheel30DistMul[8] =
+{
+  6, 4, 2, 4, 2, 4, 6, 2
+};
+
+const uint8_t wheel30DistAdd[8][8] =
+{
+  { 1, 1, 0, 1, 1, 1, 1, 1 },
+  { 2, 1, 1, 2, 0, 2, 2, 1 },
+  { 2, 2, 1, 2, 1, 1, 3, 1 },
+  { 3, 3, 1, 2, 1, 2, 4, 1 },
+  { 4, 2, 2, 2, 1, 3, 4, 1 },
+  { 5, 3, 1, 3, 2, 3, 5, 1 },
+  { 6, 4, 2, 4, 2, 4, 5, 2 },
+  { 1, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+const uint8_t wheel30Masks[8][8] =
+{
+  { BIT0, BIT4, BIT3, BIT7, BIT6, BIT2, BIT1, BIT5 },
+  { BIT1, BIT3, BIT7, BIT5, BIT0, BIT6, BIT2, BIT4 },
+  { BIT2, BIT7, BIT5, BIT4, BIT1, BIT0, BIT6, BIT3 },
+  { BIT3, BIT6, BIT0, BIT1, BIT4, BIT5, BIT7, BIT2 },
+  { BIT4, BIT2, BIT6, BIT0, BIT5, BIT7, BIT3, BIT1 },
+  { BIT5, BIT1, BIT2, BIT6, BIT7, BIT3, BIT4, BIT0 },
+  { BIT6, BIT5, BIT4, BIT3, BIT2, BIT1, BIT0, BIT7 },
+  { BIT7, BIT0, BIT1, BIT2, BIT3, BIT4, BIT5, BIT6 }
+};
+
+template <uint8_t GROUP,
+          uint8_t BASE,
           uint8_t MAX_OFFSET_ADD,
           uint8_t LOOP_ADD,
-          uint8_t DIST_ADD_0, uint8_t DIST_ADD_1, uint8_t DIST_ADD_2, uint8_t DIST_ADD_3,
-          uint8_t DIST_ADD_4, uint8_t DIST_ADD_5, uint8_t DIST_ADD_6, uint8_t DIST_ADD_7,
           uint8_t OFF_0, uint8_t OFF_1, uint8_t OFF_2, uint8_t OFF_3,
           uint8_t OFF_4, uint8_t OFF_5, uint8_t OFF_6, uint8_t OFF_7,
           uint8_t MASK_0, uint8_t MASK_1, uint8_t MASK_2, uint8_t MASK_3,
@@ -62,14 +90,20 @@ void crossOffByResidue(Vector<SievingPrime>& primes,
     std::size_t maxOffset = sievingPrime * 28 + MAX_OFFSET_ADD;
     std::size_t limit = std::max(sieveSize, maxOffset) - maxOffset;
     std::size_t loopDist = sievingPrime * 30 + LOOP_ADD;
-    std::size_t adv0 = sievingPrime * 6 + DIST_ADD_0;
-    std::size_t adv1 = sievingPrime * 4 + DIST_ADD_1;
-    std::size_t adv2 = sievingPrime * 2 + DIST_ADD_2;
-    std::size_t adv3 = sievingPrime * 4 + DIST_ADD_3;
-    std::size_t adv4 = sievingPrime * 2 + DIST_ADD_4;
-    std::size_t adv5 = sievingPrime * 4 + DIST_ADD_5;
-    std::size_t adv6 = sievingPrime * 6 + DIST_ADD_6;
-    std::size_t adv7 = sievingPrime * 2 + DIST_ADD_7;
+    const uint8_t* distAdd = wheel30DistAdd[GROUP];
+    const uint8_t* masks = wheel30Masks[GROUP];
+    std::size_t adv[8] =
+    {
+      sievingPrime * wheel30DistMul[0] + distAdd[0],
+      sievingPrime * wheel30DistMul[1] + distAdd[1],
+      sievingPrime * wheel30DistMul[2] + distAdd[2],
+      sievingPrime * wheel30DistMul[3] + distAdd[3],
+      sievingPrime * wheel30DistMul[4] + distAdd[4],
+      sievingPrime * wheel30DistMul[5] + distAdd[5],
+      sievingPrime * wheel30DistMul[6] + distAdd[6],
+      sievingPrime * wheel30DistMul[7] + distAdd[7]
+    };
+
     std::size_t s0 = sievingPrime *  0 + OFF_0;
     std::size_t s1 = sievingPrime *  6 + OFF_1;
     std::size_t s2 = sievingPrime * 10 + OFF_2;
@@ -84,18 +118,8 @@ void crossOffByResidue(Vector<SievingPrime>& primes,
     {
       wheelIndex = BASE + state;
       CHECK_FINISHED(wheelIndex);
-
-      switch (state)
-      {
-        case 1: sieve[i] &= MASK_1; i += adv1; break;
-        case 2: sieve[i] &= MASK_2; i += adv2; break;
-        case 3: sieve[i] &= MASK_3; i += adv3; break;
-        case 4: sieve[i] &= MASK_4; i += adv4; break;
-        case 5: sieve[i] &= MASK_5; i += adv5; break;
-        case 6: sieve[i] &= MASK_6; i += adv6; break;
-        case 7: sieve[i] &= MASK_7; i += adv7; break;
-        default: UNREACHABLE;
-      }
+      sieve[i] &= masks[state];
+      i += adv[state];
     }
 
     // Each iteration removes the next 8
@@ -117,19 +141,8 @@ void crossOffByResidue(Vector<SievingPrime>& primes,
     {
       wheelIndex = BASE + state;
       CHECK_FINISHED(wheelIndex);
-
-      switch (state)
-      {
-        case 0: sieve[i] &= MASK_0; i += adv0; break;
-        case 1: sieve[i] &= MASK_1; i += adv1; break;
-        case 2: sieve[i] &= MASK_2; i += adv2; break;
-        case 3: sieve[i] &= MASK_3; i += adv3; break;
-        case 4: sieve[i] &= MASK_4; i += adv4; break;
-        case 5: sieve[i] &= MASK_5; i += adv5; break;
-        case 6: sieve[i] &= MASK_6; i += adv6; break;
-        case 7: sieve[i] &= MASK_7; i += adv7; break;
-        default: UNREACHABLE;
-      }
+      sieve[i] &= masks[state];
+      i += adv[state];
     }
 
     next_iteration:;
@@ -219,64 +232,56 @@ void EratSmall::crossOff(uint8_t* sieve, std::size_t sieveSize)
 
 void EratSmall::crossOff0(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<0, 6, 7,
-                    1, 1, 0, 1, 1, 1, 1, 1,
+  crossOffByResidue<0, 0, 6, 7,
                     0, 1, 2, 2, 3, 4, 5, 6,
                     BIT0, BIT4, BIT3, BIT7, BIT6, BIT2, BIT1, BIT5>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff1(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<8, 10, 11,
-                    2, 1, 1, 2, 0, 2, 2, 1,
+  crossOffByResidue<1, 8, 10, 11,
                     0, 2, 3, 4, 6, 6, 8, 10,
                     BIT1, BIT3, BIT7, BIT5, BIT0, BIT6, BIT2, BIT4>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff2(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<16, 12, 13,
-                    2, 2, 1, 2, 1, 1, 3, 1,
+  crossOffByResidue<2, 16, 12, 13,
                     0, 2, 4, 5, 7, 8, 9, 12,
                     BIT2, BIT7, BIT5, BIT4, BIT1, BIT0, BIT6, BIT3>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff3(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<24, 16, 17,
-                    3, 3, 1, 2, 1, 2, 4, 1,
+  crossOffByResidue<3, 24, 16, 17,
                     0, 3, 6, 7, 9, 10, 12, 16,
                     BIT3, BIT6, BIT0, BIT1, BIT4, BIT5, BIT7, BIT2>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff4(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<32, 18, 19,
-                    4, 2, 2, 2, 1, 3, 4, 1,
+  crossOffByResidue<4, 32, 18, 19,
                     0, 4, 6, 8, 10, 11, 14, 18,
                     BIT4, BIT2, BIT6, BIT0, BIT5, BIT7, BIT3, BIT1>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff5(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<40, 22, 23,
-                    5, 3, 1, 3, 2, 3, 5, 1,
+  crossOffByResidue<5, 40, 22, 23,
                     0, 5, 8, 9, 12, 14, 17, 22,
                     BIT5, BIT1, BIT2, BIT6, BIT7, BIT3, BIT4, BIT0>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff6(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<48, 27, 29,
-                    6, 4, 2, 4, 2, 4, 5, 2,
+  crossOffByResidue<6, 48, 27, 29,
                     0, 6, 10, 12, 16, 18, 22, 27,
                     BIT6, BIT5, BIT4, BIT3, BIT2, BIT1, BIT0, BIT7>(primes, sieve, sieveSize);
 }
 
 void EratSmall::crossOff7(Vector<SievingPrime>& primes, uint8_t* sieve, std::size_t sieveSize)
 {
-  crossOffByResidue<56, 1, 1,
-                    1, 0, 0, 0, 0, 0, 0, 0,
+  crossOffByResidue<7, 56, 1, 1,
                     0, 1, 1, 1, 1, 1, 1, 1,
                     BIT7, BIT0, BIT1, BIT2, BIT3, BIT4, BIT5, BIT6>(primes, sieve, sieveSize);
 }
