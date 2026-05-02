66a9383b6909238e005d9655e95debdc7f9afe40
kimwalisch <kim.walisch@gmail.com>
2026-05-02 10:36:04 +0200

    Update copyright year

--- src/MemoryPool.cpp
+++ src/MemoryPool.cpp
@@ -10,7 +10,7 @@
 ///         doing any memory allocation as long as the MemoryPool's
 ///         stock is not empty.
 ///
-/// Copyright (C) 2025 Kim Walisch, <kim.walisch@gmail.com>
+/// Copyright (C) 2026 Kim Walisch, <kim.walisch@gmail.com>
 ///
 /// This file is distributed under the BSD License. See the COPYING
 /// file in the top level directory.
