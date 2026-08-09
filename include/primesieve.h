3efbbceebb25073cbc783abd5d3f1fda52830be8
Kim Walisch <kim.walisch@gmail.com>
2026-08-09 13:33:06 +0200

    Update version to 12.16

--- include/primesieve.h
+++ include/primesieve.h
@@ -15,9 +15,9 @@
 #ifndef PRIMESIEVE_H
 #define PRIMESIEVE_H
 
-#define PRIMESIEVE_VERSION "12.15"
+#define PRIMESIEVE_VERSION "12.16"
 #define PRIMESIEVE_VERSION_MAJOR 12
-#define PRIMESIEVE_VERSION_MINOR 15
+#define PRIMESIEVE_VERSION_MINOR 16
 
 #include <primesieve/iterator.h>
 
--- include/primesieve.hpp
+++ include/primesieve.hpp
@@ -13,9 +13,9 @@
 #ifndef PRIMESIEVE_HPP
 #define PRIMESIEVE_HPP
 
-#define PRIMESIEVE_VERSION "12.15"
+#define PRIMESIEVE_VERSION "12.16"
 #define PRIMESIEVE_VERSION_MAJOR 12
-#define PRIMESIEVE_VERSION_MINOR 15
+#define PRIMESIEVE_VERSION_MINOR 16
 
 #include <primesieve/iterator.hpp>
 #include <primesieve/primesieve_error.hpp>
