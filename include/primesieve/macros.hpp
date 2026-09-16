c7bc281896e8590eaab6966423073fa78dd90fe7
kimwalisch <kim.walisch@gmail.com>
2026-09-16 21:48:40 +0200

    Improve assertion output

--- include/primesieve/macros.hpp
+++ include/primesieve/macros.hpp
@@ -75,15 +75,16 @@
 #if defined(ENABLE_ASSERT)
   namespace primesieve {
   [[noreturn]]
-  void assertion_failed(const char* expression,
-                        const char* file,
-                        int line);
+  void assert_failed(const char* expression,
+                     const char* file,
+                     const char* function,
+                     int line);
   } // namespace
 
   #define ASSERT(x) \
     do { \
       if_unlikely(!(x)) \
-        primesieve::assertion_failed(#x, __FILE__, __LINE__); \
+        primesieve::assert_failed(#x, __FILE__, __func__, __LINE__); \
     } while (0)
 #else
   #define ASSERT(x) ((void) 0)
--- src/util.cpp
+++ src/util.cpp
@@ -28,14 +28,16 @@ namespace primesieve {
 /// infer that execution cannot continue after a failed assertion.
 ///
 [[noreturn]]
-void assertion_failed(const char* expression,
-                      const char* file,
-                      int line)
+void assert_failed(const char* expression,
+                   const char* file,
+                   const char* function,
+                   int line)
 {
   std::string msg("\n");
   msg += std::string(file) + ":" + std::to_string(line);
-  msg += ": assertion failed: ";
-  msg += expression + std::string("\n\n");
+  msg += ": " + std::string(function);
+  msg += ": Assertion failed: `";
+  msg += expression + std::string("'\n\n");
 
   std::cerr << msg;
 
